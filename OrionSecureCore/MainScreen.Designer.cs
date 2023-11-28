218f1b30636656cbb18438796b90f4d2105b581a
Julio José Butrón Cerpa <132603476+BlackSnake10@users.noreply.github.com>
2023-11-28 16:54:39 +0100

    Updated main file

--- OrionSecureCore/MainScreen.Designer.cs
+++ OrionSecureCore/MainScreen.Designer.cs
@@ -35,8 +35,10 @@ namespace OrionSecureCore
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "MainScreen";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
 
         }
