bdc2e19ba03557f716047c199b4d2796900c86dd
plum-bee <rafakanex1@gmail.com>
2023-12-14 16:56:15 +0100

    Quick refactor SWTextBox

--- SWUserControls/SWTextbox.cs
+++ SWUserControls/SWTextbox.cs
@@ -8,16 +8,8 @@ using System.Text.RegularExpressions;
 
 namespace SWUserControls
 {
-
-
     public class SWTextbox : TextBox
     {
-        private SWTextType textType;
-        private bool allowEmpty;
-        private bool isForeignKey;
-        private string controlName;
-
-        private static Regex regexStarWars = new Regex(@"^[BCDFGHJKLMNPQRSTVWXYZ]{4}-[0-9]{3}/[13579][AEIOU]$");
         public enum SWTextType
         {
             Number,
@@ -25,106 +17,69 @@ namespace SWUserControls
             Code
         }
 
-        public SWTextType TextType
-        {
-            get { return textType; }
-            set { textType = value; }
-        }
+        public SWTextType TextType { get; set; }
+        public bool AllowEmpty { get; set; }
+        public bool IsForeignKey { get; set; }
+        public string ControlName { get; set; }
+        public System.Drawing.Color FocusColor { get; set; } = System.Drawing.Color.Yellow;
+        public System.Drawing.Color DefaultColor { get; set; } = System.Drawing.SystemColors.Window;
 
-        public bool AllowEmpty
-        {
-            get { return allowEmpty; }
-            set { allowEmpty = value; }
-        }
-
-        public bool IsForeignKey
-        {
-            get { return isForeignKey; }
-            set { isForeignKey = value; }
-        }
-
-        public string ControlName
-        {
-            get { return controlName; }
-            set { controlName = value; }
-        }
+        private static readonly Regex regexCode = new Regex(@"^[AEIOU][A-Z]{3}[0-9]{2}[13579]$");
 
         public SWTextbox()
         {
             this.Enter += SWTextbox_Enter;
             this.Leave += SWTextbox_Leave;
-            InitializeComponent();
         }
 
         private void SWTextbox_Enter(object sender, EventArgs e)
         {
-            this.BackColor = System.Drawing.Color.Yellow;
+            this.BackColor = FocusColor;
         }
 
         private void SWTextbox_Leave(object sender, EventArgs e)
         {
-            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackColor = DefaultColor;
         }
 
-        private void InitializeComponent()
+        private void SWTextbox_Validating(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            this.SuspendLayout();
-            // 
-            // SWTextbox
-            // 
-            this.Validating += new System.ComponentModel.CancelEventHandler(this.SWTextbox_Validating);
-            this.ResumeLayout(false);
-
-        }
-
-        public void SetText()
-        {
-            Form frm = this.FindForm();
-            foreach (Control ctrl in frm.Controls)
+            if (!AllowEmpty && string.IsNullOrWhiteSpace(this.Text))
             {
-                if (ctrl.Name == controlName)
-                {
-                    ctrl.Text = this.Text;
-                }
+                ShowError("Field cannot be empty.");
+                e.Cancel = true;
+                return;
+            }
 
+            switch (TextType)
+            {
+                case SWTextType.Text:
+                    if (!Regex.IsMatch(this.Text, "^[A-Za-z]+$"))
+                    {
+                        ShowError("Invalid input for text. Please enter letters only.");
+                        e.Cancel = true;
+                    }
+                    break;
+                case SWTextType.Number:
+                    if (!Regex.IsMatch(this.Text, "^[0-9]+$"))
+                    {
+                        ShowError("Invalid input for number. Please enter digits only.");
+                        e.Cancel = true;
+                    }
+                    break;
+                case SWTextType.Code:
+                    if (!regexCode.IsMatch(this.Text))
+                    {
+                        ShowError("Invalid input for code. Please enter a valid code.");
+                        e.Cancel = true;
+                    }
+                    break;
             }
         }
 
         private void ShowError(string message)
         {
-            MessageBox.Show(message);
+            MessageBox.Show(message, "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
-
-        private void SWTextbox_Validating(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            if (isForeignKey)
-            {
-                if (!string.IsNullOrEmpty(Text))
-                {
-                    if (textType == SWTextType.Text && !Regex.IsMatch(Text, "^[A-Za-z]+$"))
-                    {
-                        ShowError("Invalid input for text. Please enter letters only.");
-                        e.Cancel = true;
-                    }
-                    else if (textType == SWTextType.Number && !Regex.IsMatch(Text, "^[0-9]+$"))
-                    {
-                        ShowError("Invalid input for numbers. Please enter digits only.");
-                        e.Cancel = true;
-                    }
-                    else if (textType == SWTextType.Code && !regexStarWars.IsMatch(Text))
-                    {
-                        ShowError("Invalid input for code. Please enter a valid code.");
-                        e.Cancel = true;
-                    }
-                    else
-                    {
-                        SetText();
-                    }
-                }
-            }
-        }
-
-
     }
-
 }
