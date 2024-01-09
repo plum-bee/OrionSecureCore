f5f1c8c2c60d199397947a6dd7c08dac68c84677
plum-bee <rafakanex1@gmail.com>
2024-01-09 16:33:58 +0100

    Added SpeciesManagement

---
+++ SpeciesManagement/SpeciesManagement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataAccess;
+
+namespace SpeciesManagement
+{
+    public partial class SpeciesManagement : BaseForm
+    {
+        public SpeciesManagement()
+        {
+            InitializeComponent();
+            SetTableName("Species");
+            SetQuery("Select * from Species");
+        }
+    }
+}
